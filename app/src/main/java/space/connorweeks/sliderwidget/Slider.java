ca6f0a79159a3339400a93ab8a29d206a9fc53b1
Connor Weeks <connorweeks1@gmail.com>
2018-02-05 16:38:22 -0700

    Ready to take to class

--- app/src/main/java/space/connorweeks/sliderwidget/Slider.java
+++ app/src/main/java/space/connorweeks/sliderwidget/Slider.java
@@ -1,11 +1,13 @@
 package space.connorweeks.sliderwidget;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.PointF;
+import android.os.Build;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -20,6 +22,18 @@ import java.util.List;
 
 public class Slider extends View
 {
+    // Make the API simpler
+    // one line in ACTION_MOVE, finding the next thumb position
+    // Use and ARC for the curve
+    // There is math on dropbox
+    // Perfect math
+    // Finger origin & finger position gives a ray
+    // Where do ray and path intersect?
+    // Make an equation for the curve, plug in the equation of the ray, solve for t
+    // t is the ray parameter
+    // r(t) = r_o + r_d * t
+    // make sure r_d is normalized ||r_d|| = 1
+
 
     private float epsilon = 4;
 
@@ -32,9 +46,6 @@ public class Slider extends View
     private PointF viewBottomRight;
     private Integer selectedCircle = null;
 
-    private float percentageOfScreen;
-
-
 
     private float radiusOfThumb;
     private float topMargin;
@@ -43,11 +54,11 @@ public class Slider extends View
     private float maxValue;
     private float verticalPadding;
     Paint myPaint;
+    Paint linePaint;
 
     private ArrayList<SliderListener> listeners;
 
-    public Slider(Context context, float thumbRadius, float verticalPadding, int numberOfThumbs, float minValue, float maxValue
-                        , float screenPercent) {
+    public Slider(Context context, float thumbRadius, float verticalPadding, int numberOfThumbs, float minValue, float maxValue) {
         super(context);
         viewTopLeft = new PointF(this.getLeft(),this.getTop());
         viewBottomRight = new PointF(this.getRight(),this.getBottom());
@@ -57,6 +68,12 @@ public class Slider extends View
         myPaint = new Paint();
         myPaint.setColor(Color.BLACK);
         myPaint.setAntiAlias(true);
+
+        linePaint = new Paint();
+        linePaint.setStyle(Paint.Style.STROKE);
+        linePaint.setColor(Color.BLACK);
+        linePaint.setStrokeWidth(3f);
+
 
         radiusOfThumb = thumbRadius;
         this.verticalPadding = verticalPadding;
@@ -69,11 +86,10 @@ public class Slider extends View
         values = new ArrayList<>();
 
         for (int i=0; i < numberOfThumbs; i++){
-            values.add(minValue);
-            circleCenters.add(new PointF(viewTopLeft.x + radiusOfThumb ,viewTopLeft.y+topMargin));
+            values.add(maxValue);
+            circleCenters.add(new PointF(viewTopLeft.x ,viewTopLeft.y));
         }
 
-        percentageOfScreen = screenPercent;
         invalidate();
     }
 
@@ -81,7 +97,7 @@ public class Slider extends View
     public void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         int width = 200;
         if ( MeasureSpec.getMode(widthMeasureSpec) != MeasureSpec.UNSPECIFIED ){
-            width = (int) (MeasureSpec.getSize(widthMeasureSpec) * percentageOfScreen / 100);
+            width = (int) (MeasureSpec.getSize(widthMeasureSpec));
         }
 
         int height = (int) (radiusOfThumb * 2f + verticalPadding * 2);
@@ -92,6 +108,7 @@ public class Slider extends View
     }
 
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected synchronized void onDraw (Canvas canvas)
     {
@@ -109,6 +126,10 @@ public class Slider extends View
         drawLineFromPoints (new PointF(viewTopLeft.x, viewTopLeft.y + topMargin),
                             new PointF(viewBottomRight.x, viewTopLeft.y + topMargin),canvas,myPaint);
 
+        drawLineFromPoints(viewTopLeft, viewBottomRight, canvas, myPaint);
+
+        canvas.drawArc(-this.getRight(), this.getTop(), this.getRight(), this.getBottom()*2, 270f, 90f, true, linePaint);
+
     }
 
     private void drawLineFromPoints(PointF viewTopLeft, PointF viewBottomRight, Canvas canvas, Paint myPaint) {
@@ -125,19 +146,17 @@ public class Slider extends View
             case MotionEvent.ACTION_MOVE:
                 // get the location of the finger down.
                 if (selectedCircle != null) {
-                    if (event.getX() < viewTopLeft.x+radiusOfThumb){
-                        circleCenters.get(selectedCircle).x = viewTopLeft.x+radiusOfThumb;
+                    float xValue = event.getX();
+                    float yValue = event.getY();
+
+                    if (event.getX() < viewTopLeft.x){
+                        xValue = viewTopLeft.x;
                     }
-                    else if (event.getX() > viewBottomRight.x-radiusOfThumb){
-                        circleCenters.get(selectedCircle).x = viewBottomRight.x-radiusOfThumb;
+                    if (event.getY() > viewBottomRight.y){
+                        yValue = viewBottomRight.y;
                     }
-                    else{
-                        circleCenters.get(selectedCircle).x = event.getX();
-                    }
-                    circleCenters.get(selectedCircle).y = viewTopLeft.y + topMargin;
-                    values.set(selectedCircle, minValue + (maxValue - minValue) *
-                            ((circleCenters.get(selectedCircle).x - (viewTopLeft.x+radiusOfThumb))
-                            / ((viewBottomRight.x-radiusOfThumb) - (viewTopLeft.x+radiusOfThumb))));
+                    calculateNewCenter(xValue, yValue, selectedCircle);
+
                     for (SliderListener l : listeners){
                         l.onValueChanged(values, this);
                     }
@@ -152,6 +171,33 @@ public class Slider extends View
 
         invalidate(); // make sure we force a redraw.
         return true;
+    }
+
+    private double length(PointF vector){
+        return Math.sqrt(Math.pow(vector.x, 2) + Math.pow(vector.y, 2));
+    }
+
+    private void calculateNewCenter(float x, float y, Integer selectedCircle) {
+        // Get a unit vector from origin to point
+        // Find the point on the parabola corresponding to the angle.
+        PointF vector = new PointF(x - this.viewTopLeft.x, y - this.viewBottomRight.y);
+        double magnitude = length(vector);
+        vector.x /= magnitude;
+        vector.y /= magnitude;
+
+        // This gives an angle from 0 degrees
+        double theta = Math.atan(Math.abs(vector.y) / Math.abs(vector.x));
+
+        float a = viewBottomRight.x - viewTopLeft.x;
+        float b = viewBottomRight.y - viewTopLeft.y;
+
+        double radius = (a*b) / Math.sqrt((Math.pow(a, 2) * Math.pow(Math.sin(theta), 2)) + (Math.pow(b, 2) * Math.pow(Math.cos(theta), 2)));
+
+        PointF newGuy = new PointF((float)(this.viewTopLeft.x + radius * Math.cos(theta)), (float)(this.viewBottomRight.y - radius * Math.sin(theta)));
+
+        circleCenters.get(selectedCircle).x = newGuy.x;
+        circleCenters.get(selectedCircle).y = newGuy.y;
+        values.set(selectedCircle, (float)(minValue + (maxValue - minValue) * theta * 180f  / (3.14159f * 90f)));
     }
 
     private double getDistanceFromCircle(float x, float y, PointF circleLocation){
